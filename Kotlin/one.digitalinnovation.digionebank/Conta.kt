6ffa2990e9eece36535883ae0d8337be4265d681
teteuds <teu.dias00@gmail.com>
2021-07-21 12:17:49 -0300

    Second Commit

---
+++ Kotlin/one.digitalinnovation.digionebank/Conta.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+class Conta(
+    val agencia: String,
+    val numer: String,
+    val saldo: BigDecimal
+){
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
--- Kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ Kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -4,11 +4,13 @@ class Pessoa {
     var nome: String = "Mateus"
     var cpf: String = "123.456.789-00" 
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(args: Array<String>) {
     val mateus = Pessoa()
-    println(mateus.nome)
-    println(mateus.cpf)
-
+    println(mateus.pessoaInfo())
 }
