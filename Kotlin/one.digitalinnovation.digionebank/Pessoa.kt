2321477b5f9e5e8e98a2db2a39fa21b2688fe150
teteuds <teu.dias00@gmail.com>
2021-07-20 23:18:49 -0300

    First

--- Kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ Kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -1,12 +1,14 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-   var nome: String = "Mateus"
-   var cpf: String = "123.456.789-00" 
+    var nome: String = "Mateus"
+    var cpf: String = "123.456.789-00" 
+    private set
 }
 
 fun main(args: Array<String>) {
     val mateus = Pessoa()
     println(mateus.nome)
     println(mateus.cpf)
+
 }
